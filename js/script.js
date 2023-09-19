3e6ae6a301d3db9af486085e671db53e016aeaaf
João Victor de Oliveira dos Santos <joaovictorfaisca@gmail.com>
2023-09-19 11:03:51 -0300

    .v3

--- js/script.js
+++ js/script.js
@@ -70,112 +70,112 @@ function TestaCPF(strCPF) {
   //tirando os pontos e o traço e atribuindo a uma variavel
   let strCPFClear = deMaskCPF(strCPF);
 
-  for (let i = 0; i < 9; i++) {
-    Soma += parseInt((strCPFClear.charAt(i))) * (10 - i);
+for (let i = 0; i < 9; i++) { //for para pegara posição do numero
+        //atribuindo soma a soma
+    Soma += parseInt((strCPFClear.charAt(i))) * (10 - i); // .charAt pega o caracter do indice especificado e começar pelo indice 0
+    Resto = (Soma * 10) % 11;
+
+    if (Resto === 10 || Resto === 11) {
+      Resto = 0;
+    }
+
+    if (Resto !== parseInt(strCPFClear.charAt(9))) {
+      return false; // pegando diretamente o digito verificador primeiro
+    }
+
+    Soma = 0;
+
+    // Verificando o segundo dígito
+
+  for (let i = 0; i < 10; i++) {  //for para pegara posição do numero
+      Soma += parseInt(strCPFClear.charAt(i)) * (11 - i); // Corrigido o comentário para "de 10 até 2"
+    }
+
+    Resto = (Soma * 10) % 11;
+
+    if (Resto === 10 || Resto === 11) {
+      Resto = 0;
+    }
+
+    if (Resto !== parseInt(strCPFClear.charAt(10))) {
+      return false; // Se a verificação falhar, retorne false imediatamente
+    }
+
+    return true;
   }
-  Resto = (Soma * 10) % 11;
+  //inicio do formulario
+  const nameInput = document.querySelector("#name");
+  const emailInput = document.querySelector("#email");
+  const cpfInput = document.querySelector("#cpf");
+  const submitButton = document.querySelector("#send");
+  const message = document.querySelector(".msg");
 
-  if (Resto === 10 || Resto === 11) {
-    Resto = 0;
+  //BOTAO PARA
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    //pegando os valores passados para os elementos
+    let nameValue = nameInput.value;
+    let emailValue = emailInput.value;
+    let cpfValue = cpfInput.value;
+
+
+    var resultado = TestaCPF(cpfValue); // passando a funcao testacpf como valor, sendo que ela recebera o cpf da pessoa
+    if (resultado === true) {
+      message.textContent = "Cadastrado com Sucesso!"
+      message.classList = "acerto";
+
+      //tempo na tela
+      setTimeout(() => {
+        message.textContent = "";
+        message.classList = "";
+      }, 10000);
+
+    } else {
+      message.textContent = "CPF Inválido! Por favor, insira um cpf válido!.";
+      message.classList = "error"; //busca na classe error
+
+      //tempo para a faixa error ficar em tela
+      setTimeout(() => {
+        message.textContent = "";
+        message.classList = "";
+        //deixando vazio o cpf
+        cpfInput.value = "";
+      }, 12000);
+    }
+
+    ///tratamento de erro
+    if (nameValue === "" || emailValue === "" || cpfValue === "") {
+      message.textContent = "Por favor, preencha todos os campos!";
+      message.classList = "error"; //busca na classe error
+
+      //tempo para a faixa error ficar em tela
+      setTimeout(() => {
+        message.textContent = "";
+        message.classList = "";
+      }, 5000);
+      return; //sai da função
+    }
+
+    //limpa todos os campos no final
+    nameInput.value = "";
+    emailInput.value = "";
+    cpfInput.value = "";
+  });
+
+
+  //colocando . e - conforme as especificações
+  function maskCPF() {
+    let strCPF = cpfInput.value;
+    if (strCPF.length == 3 || strCPF.length == 7) cpfInput.value += ".";
+    if (strCPF.length == 11) cpfInput.value += "-";
+  }
+  //retirando tudo que n seja apenas digitos
+  function deMaskCPF(cpf) {
+    return cpf.replace(/\D/g, '');
   }
 
-  if (Resto !== parseInt(strCPFClear.charAt(9))) {
-    return false; // Se a verificação falhar, retorne false imediatamente
-  }
-
-  Soma = 0;
-
-  // Verificando o segundo dígito
-
-  for (let i = 0; i < 10; i++) {
-    Soma += parseInt(strCPFClear.charAt(i)) * (11 - i); // Corrigido o comentário para "de 10 até 2"
-  }
-
-  Resto = (Soma * 10) % 11;
-
-  if (Resto === 10 || Resto === 11) {
-    Resto = 0;
-  }
-
-  if (Resto !== parseInt(strCPFClear.charAt(10))) {
-    return false; // Se a verificação falhar, retorne false imediatamente
-  }
-
-  return true;
-}
-//inicio do formulario
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const cpfInput = document.querySelector("#cpf");
-const submitButton = document.querySelector("#send");
-const message = document.querySelector(".msg");
-
-//BOTAO PARA
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  //pegando os valores passados para os elementos
-  let nameValue = nameInput.value;
-  let emailValue = emailInput.value;
-  let cpfValue = cpfInput.value;
-
-
-  var resultado = TestaCPF(cpfValue); // passando a funcao testacpf como valor, sendo que ela recebera o cpf da pessoa
-  if (resultado === true) {
-    message.textContent = "Cadastrado com Sucesso!"
-    message.classList = "acerto";
-
-    //tempo na tela
-    setTimeout(() => {
-      message.textContent = "";
-      message.classList = "";
-    }, 10000);
-
-  } else {
-    message.textContent = "CPF Inválido! Por favor, insira um cpf válido!.";
-    message.classList = "error"; //busca na classe error
-
-    //tempo para a faixa error ficar em tela
-    setTimeout(() => {
-      message.textContent = "";
-      message.classList = "";
-      //deixando vazio o cpf
-      cpfInput.value = "";
-    }, 12000);
-  }
-
-  ///tratamento de erro
-  if (nameValue === "" || emailValue === "" || cpfValue === "") {
-    message.textContent = "Por favor, preencha todos os campos!";
-    message.classList = "error"; //busca na classe error
-
-    //tempo para a faixa error ficar em tela
-    setTimeout(() => {
-      message.textContent = "";
-      message.classList = "";
-    }, 5000);
-    return; //sai da função
-  }
-
-  //limpa todos os campos no final
-  nameInput.value = "";
-  emailInput.value = "";
-  cpfInput.value = "";
-});
-
-
-//colocando . e - conforme as especificações
-function maskCPF() {
-  let strCPF = cpfInput.value;
-  if (strCPF.length == 3 || strCPF.length == 7) cpfInput.value += ".";
-  if (strCPF.length == 11) cpfInput.value += "-";
-}
-//retirando tudo que n seja apenas digitos
-function deMaskCPF(cpf) {
-  return cpf.replace(/\D/g, '');
-}
-
-// Toda vez que começar a ser inserido conteudo no cpf
-cpfInput.addEventListener("input", function () {
-  maskCPF();
-});
+  // Toda vez que começar a ser inserido conteudo no cpf
+  cpfInput.addEventListener("input", function () {
+    maskCPF();
+  });
